d2e1ac9f3ffc4c8be5dd58cc219b19c07d355391
mw1213 <33518737+mw1213@users.noreply.github.com>
2020-05-28 21:39:41 +0200

    all ui elements
    rest of labels, buttons

--- Pogoda/WilkPogoda/WilkPogoda/ViewController.swift
+++ Pogoda/WilkPogoda/WilkPogoda/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label_temp_max: UILabel!
+    
+    @IBOutlet weak var label_temp_min: UILabel!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var weather_type: UILabel!
+    
+    @IBOutlet weak var wind_speed: UILabel!
+    
+    @IBOutlet weak var air_pressure: UILabel!
+
+    @IBOutlet weak var rainfall: UILabel!
+
+    @IBOutlet weak var wind_direction: UILabel!
+
+    @IBOutlet weak var date_today: UILabel!
+    
+    @IBAction func button_next(_ sender: Any) {
+    }
+    
+    @IBAction func button_prev(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
