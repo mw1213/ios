1422f7348ddfab42b62e00a833e48576108f3898
mw1213 <33518737+mw1213@users.noreply.github.com>
2020-05-28 21:06:11 +0200

    pogoda app
    with no logic, 5 labels, 1 image, no buttons, 2 h of fighting with remote access

---
+++ Pogoda/WilkPogoda/WilkPogoda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WilkPogoda
+//
+//  Created by Student on 28/05/2020.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
